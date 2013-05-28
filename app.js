bbb60f99dd02ae9fe8a69a3e8b2d0290401083dd
Sergii Pashko <kichrum@kichrum.org.ua>
2013-05-29 00:02:51 +0300

    Transfering small files

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
+var files = [];
+//var reader = new FileReader();
+
 io.sockets.on('connection', function(socket) {
-    socket.broadcast.emit('new_user', 'connected');
+    socket.on('new_user', function(hash) {
+        socket.broadcast.emit('new_user', hash);
+    });
+    socket.on('new_file', function(data) {
+        files[data] = socket;
+        console.log(files);
+    });
+    
+    socket.on('send_file', function(data) {
+//        data.pipe(socket);
+//        socket.sendfile(data);
+//        var fs = require('fs');
+//        fs.readFile(data, function(err, content) {
+            console.log('File:');
+//           console.log(data); 
+           
+//        });
+        socket.broadcast.emit('get_file', data);
+    });
 //    socket.on('my other event', function(data) {
 //        console.log(data);
 //    });
