9d1d8dc3522b9c6c8dd45a1ff864baaffe0c31c7
Sergii Pashko <kichrum@kichrum.org.ua>
2013-06-04 23:25:40 +0300

    Universialization. Array of files ready to download. Response on resource not found. Clearence the code.

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
 var http = require('http');
 
-// Serve client side statically
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 
-var response = [];
-app.get('/content/:filename', function(req, res) {
-    res.writeHead(200);
-    console.log('File:' + req.params.filename);
-    response[req.params.filename] = res;
+var responses = [];
+var files = [];
+app.get('/content/:filename', function(request, response) {
+    if (files.indexOf(request.params.filename) === -1) {
+        response.writeHead('404');
+        response.end('Resource not found or has already downloaded once');
+        return false;
+    }
+    response.writeHead(200);
+    console.log('Requested file: ' + request.params.filename);
+    responses[request.params.filename] = response;
 });
 
 var server = http.createServer(app);
-
-// Start Binary.js server
 var BinaryServer = require('binaryjs').BinaryServer;
 var bs = BinaryServer({server: server});
 
-// Wait for new user connections
 bs.on('connection', function(client) {
-    // Incoming stream from browsers
     client.on('stream', function(stream, meta) {
-        console.log(meta.name);
-
-        var res = response[meta.name];
-        if (res) {
-            console.log('Transfer started');
-
-            stream.pipe(res, {end: false});
+        if (files.indexOf(meta.name) < 0) {
+            files.push(meta.name);
+        }
+        if (responses[meta.name]) {
+            delete files[files.indexOf(meta.name)];
+            console.log('Transfer of file ' + meta.name + ' started');
+            stream.pipe(responses[meta.name], {end: false});
             stream.on('end', function() {
-                res.end();
-                console.log('Transfer finished');
+                responses[meta.name].end();
+                console.log('Transfer of file ' + meta.name + ' finished');
             });
-            // Send progress back
             stream.on('data', function(data) {
                 stream.write({rx: data.length / meta.size});
             });
         }
-
     });
 });
 
